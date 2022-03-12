50c3afb73ba53c1bdd727e7e9c35ecf03f855837
zhusq <857355787@qq.com>
2022-03-12 22:41:49 +0800

    12

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+//
+
+
+Route::get('/', 'IndexController@index');
+Route::get('/result', 'IndexController@resultPage');

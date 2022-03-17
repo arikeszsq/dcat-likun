6a4dccf82270d7dbda4a9f77fadfebd9a22b978a
zhusq <857355787@qq.com>
2022-03-17 19:34:04 +0800

    7

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -13,24 +13,26 @@ class HomeController extends Controller
 {
     public function index(Content $content)
     {
-        return $content
-            ->header('Dashboard')
-            ->description('Description...')
-            ->body(function (Row $row) {
-                $row->column(6, function (Column $column) {
-                    $column->row(Dashboard::title());
-                    $column->row(new Examples\Tickets());
-                });
+        return $content->body('<h2 style="text-align: center;margin: 100px auto;">欢迎来到管理中心</h2>');
 
-                $row->column(6, function (Column $column) {
-                    $column->row(function (Row $row) {
-                        $row->column(6, new Examples\NewUsers());
-                        $row->column(6, new Examples\NewDevices());
-                    });
-
-                    $column->row(new Examples\Sessions());
-                    $column->row(new Examples\ProductOrders());
-                });
-            });
+//        return $content
+//            ->header('Dashboard')
+//            ->description('Description...')
+//            ->body(function (Row $row) {
+//                $row->column(6, function (Column $column) {
+//                    $column->row(Dashboard::title());
+//                    $column->row(new Examples\Tickets());
+//                });
+//
+//                $row->column(6, function (Column $column) {
+//                    $column->row(function (Row $row) {
+//                        $row->column(6, new Examples\NewUsers());
+//                        $row->column(6, new Examples\NewDevices());
+//                    });
+//
+//                    $column->row(new Examples\Sessions());
+//                    $column->row(new Examples\ProductOrders());
+//                });
+//            });
     }
 }
